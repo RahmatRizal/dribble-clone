f53a08ec6e79500699f0e7ba2063e9c2e4063068
Rahmat Rizal Andhi <boyputra03@gmail.com>
2019-03-17 16:43:11 +0700

    Set Database

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_03_17_091052) do
+ActiveRecord::Schema.define(version: 2019_03_17_093228) do
 
   create_table "impressions", force: :cascade do |t|
     t.string "impressionable_type"
@@ -38,6 +38,14 @@ ActiveRecord::Schema.define(version: 2019_03_17_091052) do
     t.index ["user_id"], name: "index_impressions_on_user_id"
   end
 
+  create_table "shots", force: :cascade do |t|
+    t.string "title"
+    t.text "description"
+    t.integer "user_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
   create_table "users", force: :cascade do |t|
     t.string "email", default: "", null: false
     t.string "encrypted_password", default: "", null: false
@@ -46,6 +54,7 @@ ActiveRecord::Schema.define(version: 2019_03_17_091052) do
     t.datetime "remember_created_at"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "name"
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
